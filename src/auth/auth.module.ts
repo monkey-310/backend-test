b288f0087b2ba98e657d880df1aa2f5131ed8092
monkey-310 <william.jin.719@gmail.com>
2025-11-21 08:24:04 -0500

    fix issue in expireOption in Auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,16 +2,18 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'secret-dev-key',
-      signOptions: {
-        expiresIn: process.env.JWT_EXPIRES_IN
-          ? parseInt(process.env.JWT_EXPIRES_IN, 10)
-          : undefined,
-      },
+    ConfigModule.forRoot(),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET') || 'secret-dev-key',
+        signOptions: { expiresIn: configService.get('JWT_EXPIRES_IN') || '1h'},
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [JwtStrategy],
